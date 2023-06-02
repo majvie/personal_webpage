e125a83bb38b08a6656d78ee606b7b135faed257
Maximilian Vieweg <maximilian.vieweg@gmail.com>
2023-06-02 12:10:48 +0200

    renamed build to docs

--- build/_app/immutable/chunks/5-aa4d3546.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/writing/_page.svelte-37b20152.js";export{t as component};
--- docs/_app/immutable/chunks/4-0c91b0be.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/resume/_page.svelte-8894b8dd.js";export{t as component};
